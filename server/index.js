d763143c0d10d05c1c13fc782d7b1be5262408df
Nitins <nitins2332@gmail.com>
2023-06-26 17:56:25 +0530

    todo-simple-project: client & server

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Todo = require("./models/Todo");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const database_url = process.env.DATABASE_URL;
+const port = process.env.PORT;
+
+mongoose
+  .connect(database_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save();
+
+  res.json(todo);
+});
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+
+  res.json(result);
+});
+
+app.get("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.complete = !todo.complete;
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.listen(port, () => console.log("server started on port 3001"));
